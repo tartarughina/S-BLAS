66c94437fc7dc8a44a7c104ddd4d6ded1c8acd6f
Riccardo Strina <riccardo.strina@icloud.com>
2024-11-24 23:23:20 +0100

    Add cleanup stages to get_matrices script

--- matrices/get_matrices.sh
+++ matrices/get_matrices.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Get archives
 wget https://suitesparse-collection-website.herokuapp.com/MM/Williams/webbase-1M.tar.gz
 wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/s3dkq4m2.tar.gz
 
+# Extract archives
 tar -xvf webbase-1M.tar.gz
 tar -xvf s3dkq4m2.tar.gz
+
+# Move matrices
+mv webbase-1M/webbase-1M.mtx .
+mv s3dkq4m2/s3dkq4m2.mtx .
+
+# Clean up
+rm -rf webbase-1M.tar.gz
+rm -rf s3dkq4m2.tar.gz
+rm -rf webbase-1M
+rm -rf s3dkq4m2
