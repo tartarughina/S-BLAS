c57a5b7fdc25ed86f42dc5cf085779f8f0de449b
Riccardo Strina <riccardo.strina@icloud.com>
2024-11-24 23:17:24 +0100

    Remove matrices and create a script to download them

---
+++ matrices/get_matrices.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://suitesparse-collection-website.herokuapp.com/MM/Williams/webbase-1M.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/GHS_psdef/s3dkq4m2.tar.gz
+
+tar -xvf webbase-1M.tar.gz
+tar -xvf s3dkq4m2.tar.gz
